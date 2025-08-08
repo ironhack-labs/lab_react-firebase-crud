76dcd4042acd782b18a388fe27cadb6955e32c15
Laura Sinclair <l.sinclair@tuta.io>
2025-08-08 17:33:02 +0200

    🎬 Solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { Link, Route, Routes } from "react-router-dom";
-import Auth from "./components/Auth";
-import MovieList from "./components/MovieList";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import "./style.css";
 import WatchListPage from "./pages/WatchListPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import "./style.css";
+
 
 function App() {
 	return (
 		<div className="App">
-			<NavBar>
-				<h1>Movie watchlist app</h1>
-				<Link to="/">Home</Link> | <Link to="/watchlist">My Watchlist</Link>
-				<Auth />
-			</NavBar>
+			<NavBar />
 			<main>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,27 @@
-function NavBar({children}) {
+import { Link, useLocation } from "react-router-dom";
+import Auth from "./Auth";
+
+function NavBar() {
+	const location = useLocation()
+	const isActive = (path) => (location.pathname === path ? "active" : "");
+
   return (
-	<nav className="navbar">
-		{children}
-	</nav>
-  )
+		<nav className="navbar">
+			<h1>Movie watchlist app</h1>
+			<div className="navbar_menu">
+				<Link to="/" className={`navbar_link ${isActive("/")}`}>
+					Home
+				</Link>
+				<Link
+					to="/watchlist"
+					className={`navbar_link ${isActive("/watchlist")}`}
+				>
+					My Watchlist
+				</Link>
+				<Auth />
+			</div>
+		</nav>
+  );
 }
 
 export default NavBar
