e9b709f9d8d2b35694d6af4bad45a497710375d1
karunya10 <karunya.prabha@gmail.com>
2025-08-13 12:01:55 +0200

    feat: initialize React project with Firebase CRUD functionality
    - Added package.json with dependencies for React, Firebase, and Vite.
    - Created main application structure with routing and protected routes.
    - Implemented authentication using Firebase with Google sign-in.
    - Developed movie management features including adding, editing, and deleting movies.
    - Created pages for login, home, and watchlist with respective components.
    - Added SVG assets for branding and icons.
    - Configured Vite for development and production builds.

---
+++ src/components/Auth.jsx
@@ -0,0 +1,29 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, signInWithGoogle } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+
+function Auth() {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  if (user !== null) {
+    navigate("/homePage");
+  }
+
+  return (
+    <div>
+      {loading && <p>Loading...</p>}
+      {user === null && (
+        <button
+          onClick={async () => {
+            await signInWithGoogle();
+            navigate("/homePage");
+          }}
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Auth;
